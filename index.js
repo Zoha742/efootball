6af1730994cdcaa016463f62d8f0ad9cee374597
Zoha742 <arefinzoha23@gmail.com>
2026-04-05 18:16:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+require('dotenv').config(); // এটি সবার উপরে দাও
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
 const cloudinary = require('cloudinary').v2;
 
 // Cloudinary কনফিগারেশন
