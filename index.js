832058e968354ba3d4f90f43d2acee2df73777c8
Zoha742 <arefinzoha23@gmail.com>
2026-04-05 19:08:52 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ const cloudinary = require('cloudinary').v2;
 // Cloudinary কনফিগারেশন
 cloudinary.config({ 
   cloud_name: 'dyqegwdme', 
-  api_key: '485815672387742ে', 
-  api_secret: 'FSQ8cdLrm7s20jZL9gUDr1cSeX8ে' 
+  api_key: '485815672387742', 
+  api_secret: 'FSQ8cdLrm7s20jZL9gUDr1cSeX8' 
 });
