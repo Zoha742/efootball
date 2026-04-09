d929621f3feb2bdd6e1165b709228a6f1d651c68
Zoha742 <arefinzoha23@gmail.com>
2026-04-09 18:35:49 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 
-app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
 const DATA_PATH = path.join(__dirname, 'players.json');
 
-// ডাটা লোড করার API
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// JSON data route (cite: Part 2-4 support)
 app.get('/api/data', (req, res) => {
     fs.readFile(DATA_PATH, 'utf8', (err, data) => {
         if (err) return res.status(500).json({ error: "Read Error" });
         res.json(JSON.parse(data));
     });
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
