ce18a5e49c283365ac7f49d761ba13fd88f3dcff
Zoha742 <arefinzoha23@gmail.com>
2026-05-02 02:07:43 +0600

    Update ads-manager2.js

--- public/ads-manager2.js
+++ public/ads-manager2.js
@@ -1,31 +1,39 @@
 const BioAdController = {
     videoBlockId: "29212", 
-    bannerBlockId: "29213",
+    bannerBlockId: "29213", // ব্যানার আইডি ফরম্যাট ঠিক করা হয়েছে
 
-    _canShow: function(key) {
+    // ১২ ঘণ্টার চেক লজিক (শুধু ভিডিওর জন্য)
+    _canShowVideo: function(key) {
         const last = localStorage.getItem('ad_' + key);
         const now = Date.now();
-        return !last || (now - last) > (12 * 60 * 60 * 1000);
+        const twelveHours = 12 * 60 * 60 * 1000;
+        return !last || (now - last) > twelveHours;
     },
 
+    // ভিডিও অ্যাড - ১২ ঘণ্টায় একবার আসবে
     showBioAd: function(pageName) {
-        if (!this._canShow(pageName)) {
-            console.log("Limit active: Skip video for " + pageName);
+        if (!this._canShowVideo(pageName)) {
+            console.log("Video Limit: Skipping for " + pageName);
             return;
         }
 
         if (window.Adsgram) {
-            const ad = window.Adsgram.init({ blockId: this.videoBlockId });[cite: 4]
+            const ad = window.Adsgram.init({ blockId: this.videoBlockId });
             ad.show().then(() => {
                 localStorage.setItem('ad_' + pageName, Date.now());
+                console.log("Video Ad Success");
             }).catch(err => console.log("Video Error", err));
         }
     },
 
+    // ব্যানার অ্যাড - এটি সব সময় লোড হবে
     loadBioBanner: function(divId) {
-        if (window.Adsgram && document.getElementById(divId)) {
-            const banner = window.Adsgram.init({ blockId: this.bannerBlockId });[cite: 4]
-            banner.show().catch(e => console.log("Banner Fail", e));
+        const container = document.getElementById(divId);
+        if (window.Adsgram && container) {
+            const banner = window.Adsgram.init({ blockId: this.bannerBlockId });
+            banner.show()
+                .then(() => console.log("Banner Loaded (Always)"))
+                .catch(e => console.log("Banner Fail", e));
         }
     }
 };
